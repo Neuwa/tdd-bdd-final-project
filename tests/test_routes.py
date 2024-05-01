8893577bda2fd3d4a52f00d651d9b9fa57e2d2d4
neuwa <y.liverdoold@gmail.com>
2024-05-01 07:51:49 -0400

    almost done, with mistake list_by_name,category,availability

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -203,6 +203,22 @@ class TestProductRoutes(TestCase):
         updated_product = response.get_json()
         self.assertEqual(updated_product["description"], "unknown")
 
+    def test_update_product_without_id(self):
+        """It should not Update an Product without id"""
+        # create a product to update
+        test_product = ProductFactory()
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # update the product
+        new_product = response.get_json()
+        new_product["id"] = None
+        new_product["description"] = "unknown"
+        response = self.client.put(f"{BASE_URL}/{new_product['id']}", json=new_product)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+
     #-----------------------------------
     # TEST DELETE A PRODUCT
     #-----------------------------------
